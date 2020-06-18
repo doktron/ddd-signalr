ba03bdcc408334ea261dd4cace39245faa37c197
Peter Ferngren <peter.ferngren@datscha.com>
2020-06-18 15:28:46 +0200

    Differentiate clients and helpdesk

--- ClientServer/Hubs/HelpdeskHub.cs
+++ ClientServer/Hubs/HelpdeskHub.cs
@@ -8,9 +8,41 @@ namespace ClientServer.Hubs
 {
 	public class HelpdeskHub : Hub
 	{
+		private static readonly List<string> ConnectedClients = new List<string>();
+
 		public async Task SendMessage(string user, string message)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message);
+			await Clients.Groups("HelpdeskClients").SendAsync("ReceiveMessage", Context.ConnectionId, message);
+			await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+		}
+
+		public async Task SendHelpdeskMessage(string connectionId, string user, string message)
+		{
+			await Clients.Groups(connectionId, "HelpdeskClients").SendAsync("ReceiveMessage", user, message);
+		}
+
+		public async Task RegisterWebClient()
+		{
+			ConnectedClients.Add(Context.ConnectionId);
+			await Groups.AddToGroupAsync(Context.ConnectionId, Context.ConnectionId);
+			await Clients.Groups("HelpdeskClients").SendAsync("ClientListUpdate", ConnectedClients);
+			await Clients.Caller.SendAsync("ReceiveMessage", "Helpdesk", "Hello, how can I help you?");
+		}
+
+		public async Task RegisterHelpdeskClient()
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, "HelpdeskClients");
+			await Clients.Caller.SendAsync("ClientListUpdate", ConnectedClients);
+		}
+
+		public override async Task OnDisconnectedAsync(Exception exception)
+		{
+			if (ConnectedClients.Contains(Context.ConnectionId))
+			{
+				ConnectedClients.Remove(Context.ConnectionId);
+				await Clients.Groups("HelpdeskClients").SendAsync("ClientListUpdate", ConnectedClients);
+			}
+			await base.OnDisconnectedAsync(exception);
 		}
 	}
 }
--- ClientServer/wwwroot/js/helpdesk.js
+++ ClientServer/wwwroot/js/helpdesk.js
@@ -8,6 +8,7 @@ var connection = new signalR.HubConnectionBuilder()
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
+	console.log("received message", user, message);
 	var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 	var encodedMsg = user + " says " + msg;
 	var li = document.createElement("li");
@@ -15,17 +16,62 @@ connection.on("ReceiveMessage", function (user, message) {
 	document.getElementById("messagesList").appendChild(li);
 });
 
+connection.on("ClientListUpdate",
+	function(clients) {
+		var clientInput = document.getElementById("clientInput");
+		clientInput.options.length = 0;
+		for (var i = 0; i < clients.length; i++) {
+			clientInput.options[i] = new Option(clients[i], clients[i]);
+		}
+	});
+
 connection.start().then(function () {
-	document.getElementById("sendButton").disabled = false;
+	//document.getElementById("sendButton").disabled = false;
+	console.log("connected");
 }).catch(function (err) {
 	return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
+function sendMessage(event) {
 	var user = document.getElementById("userInput").value;
 	var message = document.getElementById("messageInput").value;
 	connection.invoke("SendMessage", user, message).catch(function (err) {
 		return console.error(err.toString());
+	});
+	event.preventDefault();
+}
+
+function sendHelpdeskMessage(event) {
+	var clientInput = document.getElementById("clientInput");
+	var connectionId = clientInput.options[clientInput.selectedIndex].value;
+	var user = document.getElementById("userInput").value;
+	var message = document.getElementById("messageInput").value;
+	connection.invoke("SendHelpdeskMessage", connectionId, user, message).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+}
+
+document.getElementById("registerClientButton").addEventListener("click", function (event) {
+	console.log("Register client");
+	connection.invoke("RegisterWebClient").then(function() {
+		document.getElementById("sendButton").disabled = false;
+		document.getElementById("registerClientButton").disabled = true;
+		document.getElementById("registerHelpdeskButton").disabled = true;
+		document.getElementById("sendButton").addEventListener("click", sendMessage);
+		
+	});
+	event.preventDefault();
+});
+
+document.getElementById("registerHelpdeskButton").addEventListener("click", function (event) {
+	console.log("Register helpdesk");
+	connection.invoke("RegisterHelpdeskClient").then(function () {
+		document.getElementById("sendButton").disabled = false;
+		document.getElementById("registerClientButton").disabled = true;
+		document.getElementById("registerHelpdeskButton").disabled = true;
+		document.getElementById("sendButton").addEventListener("click", sendHelpdeskMessage);
+		document.getElementById("clientRow").style.display = "block";
 	});
 	event.preventDefault();
 });
